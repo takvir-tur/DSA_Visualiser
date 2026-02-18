675152bd95ec16af6f55315369674869f64f3023
takvir-tur <takvirhossain23@gmail.com>
2026-02-18 16:12:17 +0600

    clicks_added

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -3,12 +3,36 @@ package com.example.demo1;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import javafx.scene.input.MouseEvent;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void gotoSorting(MouseEvent click){
+        System.out.println("sort_window");
     }
+    @FXML
+    void gotoArray(MouseEvent click){
+        System.out.println("array_window");
+    }
+    @FXML
+    void gotoLinkedList(MouseEvent click){
+        System.out.println("LL_window");
+    }
+    @FXML
+    void gotoStack(MouseEvent click){
+        System.out.println("stack_window");
+    }
+    @FXML
+    void gotoQueue(MouseEvent click){
+        System.out.println("queue_window");
+    }
+    @FXML
+    void gotoGraph(MouseEvent click){
+        System.out.println("graph_window");
+    }
+
+
 }
